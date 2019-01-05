7a5f69c868eadddbfc4f4f37fa36198dee3a53fc
Manuel Fragoso <fragoso.manuel@gmail.com>
2019-01-05 17:33:35 -0600

    Example of Oracle db connection Version 1

---
+++ DBexample/Program.cs
@@ -0,0 +1,54 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DBexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DB Example");
+
+            String connectionString = "DATA SOURCE = XE; PASSWORD=abc123; USER ID = HR;";
+
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = connectionString;
+            con.Open();
+            Console.WriteLine("Connection is opened");
+
+            OracleCommand query = new OracleCommand();
+            String employeeId = "105";
+            query.CommandText = "SELECT LAST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID = " + employeeId;
+            
+
+            query.Connection = con;
+            query.CommandType = CommandType.Text;
+            
+
+            OracleDataReader dr = query.ExecuteReader();
+            dr.Read();
+            Console.WriteLine("dr.Read() is done");
+
+            String result;
+            result = dr.GetString(0);
+
+            Console.WriteLine("The result of query is: " + result);
+
+            con.Close();
+
+            Console.WriteLine("End of program. Good Bye");
+
+
+
+
+
+
+        }
+    }
+}
